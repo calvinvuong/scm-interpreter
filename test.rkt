0ba6c3e2d8a51bd5f3ec27825e4657ea758566f1
Ben <the12thbenyoung@gmail.com>
2019-02-16 20:47:02 -0500

    implement math-eval-cps

--- test.rkt
+++ test.rkt
@@ -11,17 +11,47 @@
   (lambda (lis)
     (list-length-cps lis (lambda (v) v))))
 
-(define mathEval 
-  (lambda (exp return)
+(define get-operator car)
+(define exp1 cadr)
+(define exp2 caddr)
+
+(define math-eval-cps
+  (lambda (expr return)
     (cond 
-      [(null? exp) (error 'undefined "Well hey jimbo that's not a valid expression")]
-      [(number? exp) (return exp)]
-      ;; if next expression's length is 2, it's unary -
-      [(and (eq? (list-length lis) 2)
-            (eq? (car lis) '-))
-       (mathEval (cdr exp) (lambda (v) (return (* -1 v))))
-      [(eq? (list-length lis) 2) (error 'undefined "Well hey jimbo that's not a valid expression")]
-      [(not (eq? 
+      [(null? expr) (error 'undefined "ya know jimbo that's not a valid expression")]
+      [(number? expr) (return expr)]
+      ;; if next exprression's length is 2, it's unary -
+      [(and (eq? (list-length expr) 2)
+            (eq? (get-operator expr) '-))
+       (math-eval-cps (exp1 expr) (lambda (v) (return (* -1 v))))]
+      [(eq? (list-length expr) 2) (error 'undefined "ya know jimbo that's not a valid exprression")]
+      [(not (eq? (list-length expr) 3)) (error 'undefined "ya know jimbo that's not a valid exprression")]
+      [(eq? (get-operator expr) '+) 
+       (math-eval-cps (exp1 expr) 
+                 (lambda (v1) (math-eval-cps (exp2 expr) 
+                                        (lambda (v2) (return (+ v1 v2))))))]
+      [(eq? (get-operator expr) '-) 
+       (math-eval-cps (exp1 expr) 
+                 (lambda (v1) (math-eval-cps (exp2 expr) 
+                                        (lambda (v2) (return (- v1 v2))))))]
+      [(eq? (get-operator expr) '*) 
+       (math-eval-cps (exp1 expr) 
+                 (lambda (v1) (math-eval-cps (exp2 expr) 
+                                        (lambda (v2) (return (* v1 v2))))))]
+      [(eq? (get-operator expr) '/) 
+       (math-eval-cps (exp1 expr) 
+                 (lambda (v1) (math-eval-cps (exp2 expr) 
+                                        (lambda (v2) (return (quotient v1 v2))))))]
+      [(eq? (get-operator expr) '%) 
+       (math-eval-cps (exp1 expr) 
+                 (lambda (v1) (math-eval-cps (exp2 expr) 
+                                        (lambda (v2) (return (remainder v1 v2))))))])))
+
+(define math-eval
+  (lambda (expr) 
+    (math-eval-cps expr (lambda (v) v))))   
+
+(define 
        
 ;; Add a binding to the state
 (define add-to-state
