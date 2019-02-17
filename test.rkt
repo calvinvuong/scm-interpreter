64b2ead01dfc7ccea4b98b35f348905e642c8996
Ben <the12thbenyoung@gmail.com>
2019-02-16 22:21:52 -0500

    Implement non-state part of M-boolean

--- test.rkt
+++ test.rkt
@@ -51,7 +51,6 @@
   (lambda (expr) 
     (math-eval-cps expr (lambda (v) v))))   
 
-(define 
        
 ;; Add a binding to the state
 (define add-to-state
@@ -66,5 +65,45 @@
       [(eq? (car state) a)  (return (cdr state))]
       [else                 (remove-from-state var (cdr state) (lambda (v) (return (cons (car lis) v))))])))
 
-(define  )
+(define (atom? x) (not (or (pair? x) (null? x))))
+
+(define M-boolean-cps
+  (lambda (expr state return)
+    (cond
+      [(null? expr) (error 'undefined "ya know jimbo that's not a valid expression")]
+      [(atom? expr) (error 'undefined "ya know jimbo that's not a valid expression")]
+      [(eq? (get-operator expr) '==) 
+       (return (eq? (math-eval (exp1 expr)) (math-eval (exp2 expr))))]
+      [(eq? (get-operator expr) '!=) 
+       (return (not (eq? (math-eval (exp1 expr)) (math-eval (exp2 expr)))))]
+      [(eq? (get-operator expr) '<) 
+       (return (< (math-eval (exp1 expr)) (math-eval (exp2 expr))))]
+      [(eq? (get-operator expr) '>) 
+       (return (> (math-eval (exp1 expr)) (math-eval (exp2 expr))))]
+      [(eq? (get-operator expr) '>=) 
+       (return (>= (math-eval (exp1 expr)) (math-eval (exp2 expr))))]
+      [(eq? (get-operator expr) '<=) 
+       (return (<= (math-eval (exp1 expr)) (math-eval (exp2 expr))))]
+      [(eq? (get-operator expr) '&&) 
+       (M-boolean-cps (exp1 expr) 
+                      state
+                      (lambda (v1) (M-boolean-cps (exp2 expr) 
+                                                  state
+                                                  (lambda (v2) (return (and v1 v2))))))]
+      [(eq? (get-operator expr) '||) 
+       (M-boolean-cps (exp1 expr) 
+                      state
+                      (lambda (v1) (M-boolean-cps (exp2 expr) 
+                                                  state
+                                                  (lambda (v2) (return (or v1 v2))))))]
+      [(eq? (get-operator expr) '!) 
+       (M-boolean-cps (exp1 expr) 
+                      state
+                      (lambda (v) (return (not v))))])))
+      
+
+(define M-boolean
+  (lambda (expr state)
+    (M-boolean-cps expr state (lambda (v) v))))
+
 
