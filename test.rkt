e34f63f311523e4e4cf4f476d81b79c75166b0f2
Calvin Vuong <cvuong@stuy.edu>
2019-02-16 22:23:31 -0500

    slight update to naming

--- test.rkt
+++ test.rkt
@@ -69,31 +69,31 @@
 ;; wrapper function
 (define remove-from-state
   (lambda (var state)
-    (remove-from-state-helper var (car state) (cadr state) (lambda (v) v))))
+    (remove-from-state-cps var (car state) (cadr state) (lambda (v) v))))
 
 ;; s1 are the variable names
 ;; s2 are the values
-(define remove-from-state-helper
+(define remove-from-state-cps
   (lambda (var s1 s2 return)
     (cond
       [(null? s1)         (return '( () () ))]
-      [(eq? (car s1) var) (remove-from-state-helper var (cdr s1) (cdr s2) (lambda (v) (return (cons (car v) (list (cadr v))))))]
-      [else               (remove-from-state-helper var (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons (car s2) (cadr v)))))))])))
+      [(eq? (car s1) var) (remove-from-state-cps var (cdr s1) (cdr s2) (lambda (v) (return (cons (car v) (list (cadr v))))))]
+      [else               (remove-from-state-cps var (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons (car s2) (cadr v)))))))])))
 
 ;; update a variable value in state
 ;; wrapper function
 (define update-binding
   (lambda (var val state)
-    (update-binding-helper var val (car state) (cadr state) (lambda (v) v))))
+    (update-binding-cps var val (car state) (cadr state) (lambda (v) v))))
 
 ;; s1 are the variable names
 ;; s2 are the values
-(define update-binding-helper
+(define update-binding-cps
   (lambda (var val s1 s2 return)
     (cond
       [(null? s1)         (return '( () () ))]
-      [(eq? (car s1) var) (update-binding-helper var val (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons val (cadr v)))))))]
-      [else               (update-binding-helper var val (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons (car s2) (cadr v)))))))])))
+      [(eq? (car s1) var) (update-binding-cps var val (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons val (cadr v)))))))]
+      [else               (update-binding-cps var val (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons (car s2) (cadr v)))))))])))
 
     
     
