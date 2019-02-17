ea9640160b36b4005d2f8f79bc2b84fbe14ca326
Calvin Vuong <cvuong@stuy.edu>
2019-02-16 22:21:12 -0500

    add-to-state, remove-from-state, update-binding implemented in cps

--- test.rkt
+++ test.rkt
@@ -1,3 +1,5 @@
+#lang racket
+
 (require "simpleParser.rkt")
 (parser "testcode")
 
@@ -49,22 +51,53 @@
 
 (define math-eval
   (lambda (expr) 
-    (math-eval-cps expr (lambda (v) v))))   
+    (math-eval-cps expr (lambda (v) v))))    
 
-(define 
-       
+
+
 ;; Add a binding to the state
+;; takes a list of two elements: key value pair
+;; max 1 recursion: cps not necessary
 (define add-to-state
-  (lambda (var val state)
-    (cons (var val) state)))
+  (lambda (binding state)
+    (if (null? state)
+        '()
+        (cons (cons (car binding) (car state)) (add-to-state (cdr binding) (cdr state))))))
+      
 
-; Remove binding from state
+;; Remove binding from state
+;; wrapper function
 (define remove-from-state
-  (lambda (var state return)
-    (cond
-      [(null? state)        (return '())]
-      [(eq? (car state) a)  (return (cdr state))]
-      [else                 (remove-from-state var (cdr state) (lambda (v) (return (cons (car lis) v))))])))
+  (lambda (var state)
+    (remove-from-state-helper var (car state) (cadr state) (lambda (v) v))))
 
-(define  )
+;; s1 are the variable names
+;; s2 are the values
+(define remove-from-state-helper
+  (lambda (var s1 s2 return)
+    (cond
+      [(null? s1)         (return '( () () ))]
+      [(eq? (car s1) var) (remove-from-state-helper var (cdr s1) (cdr s2) (lambda (v) (return (cons (car v) (list (cadr v))))))]
+      [else               (remove-from-state-helper var (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons (car s2) (cadr v)))))))])))
+
+;; update a variable value in state
+;; wrapper function
+(define update-binding
+  (lambda (var val state)
+    (update-binding-helper var val (car state) (cadr state) (lambda (v) v))))
+
+;; s1 are the variable names
+;; s2 are the values
+(define update-binding-helper
+  (lambda (var val s1 s2 return)
+    (cond
+      [(null? s1)         (return '( () () ))]
+      [(eq? (car s1) var) (update-binding-helper var val (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons val (cadr v)))))))]
+      [else               (update-binding-helper var val (cdr s1) (cdr s2) (lambda (v) (return (cons (cons (car s1) (car v)) (list (cons (car s2) (cadr v)))))))])))
+
+    
+    
+
+
+
 
