e66efec40c3476338116e835fa86d67fc61e5a54
Ben <the12thbenyoung@gmail.com>
2019-02-18 23:20:05 -0500

    Implement variable redeclaration error

--- interpreter.rkt
+++ interpreter.rkt
@@ -8,16 +8,19 @@
 
 (define initialState '(() ()))
 
+;;calls interpret-tree on parsed code
 (define interpret
   (lambda (filename)
     (interpret-tree (parser filename) initialState)))
 
+;;interprets tree from parser
 (define interpret-tree
   (lambda (tree state)
     (cond
       [(null? tree) (translate-boolean (get-var-value state 'return))]
       [else (interpret-tree (cdr tree) (M-state (car tree) state))])))
 
+;;turns #f and #t into 'false and 'true for final return
 (define translate-boolean
   (lambda (x)
     (cond
@@ -25,6 +28,7 @@
       [(eq? x #t) 'true]
       [else x])))
 
+;;returns length of lis
 (define list-length-cps
   (lambda (lis return)
     (cond
@@ -35,6 +39,7 @@
   (lambda (lis)
     (list-length-cps lis (lambda (v) v))))
 
+;;returns value of variable in current state
 (define get-var-value
   (lambda (state var)
     (cond
@@ -43,6 +48,7 @@
       [(eq? (caar state) var)                                 (caadr state)]
       [else                                                   (get-var-value (cons (cdar state) (list (cdr (car (cdr state))))) var)])))
 
+;;returns numeric or boolean value of expression
 (define M-value-cps
   (lambda (expr state return)
     (cond 
@@ -94,11 +100,13 @@
                                               (lambda (v2) (return (remainder v1 v2))))))]
       [else                              (return (M-boolean expr state))])))
 
+;;calls M-value-cps
 (define M-value
   (lambda (expr state) 
     (M-value-cps expr state (lambda (v) v)))) 
     
 
+;;returns whether an expression is true or false
 (define M-boolean-cps
   (lambda (expr state return)
     (cond
@@ -130,7 +138,7 @@
                       state
                       (lambda (v) (return (not v))))])))
       
-
+;;calls M-boolean-cps
 (define M-boolean
   (lambda (expr state)
     (M-boolean-cps expr state (lambda (v) v))))
@@ -160,13 +168,13 @@
       [(eq? (car s1) var) (remove-from-state-cps var
                                                  (cdr s1)
                                                  (cdr s2)
-                                                 (lambda (v) (return (cons (car v) (list (cadr v))))))]
+                                                 (lambda (v) (return (cons (car v) 
+                                                                           (list (cadr v))))))]
       [else               (remove-from-state-cps var
                                                  (cdr s1)
                                                  (cdr s2)
-                                                 (lambda (v) (return (cons
-                                                                      (cons (car s1) (car v))
-                                                                      (list (cons (car s2) (cadr v)))))))])))
+                                                 (lambda (v) (return (cons (cons (car s1) (car v))
+                                                                           (list (cons (car s2) (cadr v)))))))])))
 
 ;; update a variable value in state
 ;; wrapper function
@@ -195,6 +203,7 @@
                                                                    (cons (car s1) (car v))
                                                                    (list (cons (car s2) (cadr v)))))))])))
 
+;;calls one of many M-state-** functions depending on nature of input
 (define M-state
   (lambda (expr state)
     (cond
@@ -206,18 +215,38 @@
       [(eq? (get-keyword expr) 'while)      (M-state-while expr state)]
       [else                                 state])))
 
+;;helper for var-declared: checked if atom is in list
+(define contains
+  (lambda (a lis)
+    (cond
+      [(null? lis) #f]
+      [(eq? a (car lis)) #t]
+      [else (contains a (cdr lis))]))) 
+
+;;checks to see if var is arleady declared in this state
+;;For use in M-state-declare
+(define var-declared
+  (lambda (var state)
+    (contains var (car state)))) 
+
+;;Updates state for a variable declaration
 (define M-state-declare
   (lambda (expr state)
     (cond
-      [(eq? (list-length expr) 3)           (add-to-state (cons (declare-var expr) (list (M-value (caddr expr) state))) state)]
-      [(not (eq? (list-length expr) 2))     (error 'error "Invalid declare expression.")]
-      [else                                 (add-to-state (cons (declare-var expr) '(null)) state)])))
+      [(var-declared (declare-var expr) state)     (error 'error "Variable already declared!")]
+      [(eq? (list-length expr) 3)                  (add-to-state (cons (declare-var expr) 
+                                                                       (list (M-value (caddr expr) state))) 
+                                                                 state)]
+      [(not (eq? (list-length expr) 2))            (error 'error "Invalid declare expression.")]
+      [else                                        (add-to-state (cons (declare-var expr) '(null))
+                                                                 state)])))
 
 ;; updates the state in variable assignment
 (define M-state-assign
   (lambda (expr state)
     (if (eq? (list-length expr) 3)
-        (update-binding (exp1 expr) (M-value (exp2 expr) state) state)
+        (update-binding (exp1 expr) 
+                        (M-value (exp2 expr) state) state)
         (error 'error "Invalid assign."))))
 
 ;; return does not update state, so just pass control to M-value
@@ -236,21 +265,21 @@
 (define M-state-if
   (lambda (expr state)
     (cond
-      [(eq? (M-boolean (conditional expr) state) #t) (M-state (body expr) (M-state (conditional expr) state))]
-      [(> (list-length expr) 3)                      (M-state (rest-if expr) (M-state (conditional expr) state))]
-      [else                                          (M-state (conditional expr) state)])))
+      [(eq? (M-boolean (conditional expr) state) #t) (M-state (body expr) 
+                                                              (M-state (conditional expr) state))]
+      [(> (list-length expr) 3)                      (M-state (rest-if expr) 
+                                                              (M-state (conditional expr) state))]
+      [else                                          (M-state (conditional expr) 
+                                                              state)])))
 
-;;    (if (eq? (M-boolean (conditional expr) state) #t)
-;;        (M-state (body expr) (M-state (conditional expr) state)) ; return updated state from executing body and evaluating conditional
-;;        (M-state (conditional expr) state)))) ; return updated state from evaluating conditional
-        
 
 ;; update state while loop
-;; handles side effects
 (define M-state-while
   (lambda (expr state)
     (if (eq? (M-boolean (conditional expr) state) #t)
-        (M-state-while expr (M-state (body expr) (M-state (conditional expr) state)))
+        (M-state-while expr 
+                       (M-state (body expr) 
+                                (M-state (conditional expr) state)))
         (M-state (conditional expr) state))))
  
 
